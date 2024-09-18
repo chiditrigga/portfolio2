63bd2e29631853f7d9cb1f42079173b036347d38
daniel <chididaniel1998@gmail.com>
2024-09-18 16:56:59 -0700

    first

---
+++ app/projects/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className=' bg-black  text-white'>
+
+         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda sunt quidem explicabo. Odio tenetur nesciunt dolorum fugiat cupiditate, rerum beatae, vitae exercitationem assumenda soluta consectetur maxime inventore sequi perspiciatis nulla. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe quisquam assumenda sequi magnam sint suscipit hic ea, accusamus maiores. Omnis enim rerum placeat dolorum, excepturi at aut quam magnam sunt? Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, voluptatibus ad! Velit voluptate ducimus deserunt! Iure repudiandae nulla aut voluptas dolor, quae aspernatur perferendis, ducimus sapiente vero facilis dignissimos labore!
+
+
+
+
+         
+    </div>
+  )
+}
+
+export default page
